0e3cd732420ca4b1440fe177a13a8313505e5805
mahitha-kusumanchi <167613798+mahitha-kusumanchi@users.noreply.github.com>
2024-11-06 14:58:26 +0530

    Create detectCycleBFS.py

---
+++ detectCycleBFS.py
@@ -0,0 +1,22 @@
+from collections import deque
+def detect(src,visited,arr):
+    q=deque([(src,-1)])
+    visited[src]=1
+    while q:
+        node,parent=q.popleft()
+        for neighbor in arr[node]:
+            if visited[neighbor]==0:
+                visited[neighbor]=1
+                q.append((neighbor,node))
+            elif(parent!=neighbor):
+                return True
+    return False
+def detectCycle(arr):
+    visited=[0]*len(arr)
+    for i in range(len(arr)):
+        if visited[i]==0:
+            if detect(i,visited,arr):
+                return True
+    return False
+adj = [[], [2], [1,3], [2]]
+print(detectCycle(adj))
